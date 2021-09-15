023cf15fcbcad124db4f4838f7d8c3b9d88a778b
misbahhassan08 <misbahhassan08@gmail.com>
2021-09-15 16:12:31 +0500

    website basics

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBm9aqlnW3kprayTQRXPM_DXcB3Q3vy45E",
+    authDomain: "synapses-323217.firebaseapp.com",
+    databaseURL: "https://synapses-323217-default-rtdb.firebaseio.com",
+    projectId: "synapses-323217",
+    storageBucket: "synapses-323217.appspot.com",
+    messagingSenderId: "425490400637",
+    appId: "1:425490400637:web:b255d2603e47dfa8673814",
+    measurementId: "G-4BZDVWHKZD"
+  };
